78b9eb973bc6db4c6066aa8f9b30cda4cdbcbe5b
Jeremy Broutin <jeremybroutin@google.com>
2017-05-21 10:41:07 -0700

    Initial Commit

---
+++ MyHoodObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyHoodObjC
+//
+//  Created by Jeremy Broutin on 5/21/17.
+//  Copyright © 2017 Jeremy Broutin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyHoodObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyHoodObjC
+//
+//  Created by Jeremy Broutin on 5/21/17.
+//  Copyright © 2017 Jeremy Broutin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
